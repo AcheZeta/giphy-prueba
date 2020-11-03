a2ebd81c7fbb828ba13d11b6b82b2756ba31afae
AcheZeta <ache.mariana@gmail.com>
2020-11-03 15:44:52 -0600

    style

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div id="app" class="h-24 bg-gradient-to-r from-purple-200 to-pink-400">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
-    <div ></div>
-    <!-- Search -->
-    <input type="text" name="keyword" v-model="query" @keydown="getGifs"
-    class="border py-2 px-5 text-grey-darkest shadow-inner">
-    <!-- Trending -->
-    <div v-for="gif in gifdata" :key="gif.id">
-      <img :src="gif.images.original.url" :alt="gif.title">
-    </div>
-    <router-view/>
+    </div> -->
+    <main>
+      <section id="search-section">
+        <!-- Search -->
+        <label for="gifsearch">Busca un Gif</label>
+        <input
+          id="gifsearch"
+          type="text"
+          name="keyword"
+          v-model="query"
+          @keydown="getGifs"
+          class="border rounded-lg py-2 px-5 text-grey-darkest shadow-inner m-8"
+          placeholder="cat"
+        />
+      </section>
+      <!-- Trending -->
+      <section
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4"
+      >
+        <figure v-for="gif in gifdata" :key="gif.id" class="p-4">
+          <img
+            class="rounded-lg"
+            :src="gif.images.original.url"
+            :alt="gif.title"
+          />
+        </figure>
+      </section>
+    </main>
+    <router-view />
   </div>
 </template>
 
@@ -34,11 +53,11 @@ export default {
   mounted() {},
   methods: {
     getGifs() {
-      const url = `${this.url}api_key=${this.key}&q=${this.query}&limit=4`;
+      const url = `${this.url}api_key=${this.key}&q=${this.query}&limit=10`;
       fetch(url)
         .then((response) => response.json())
         // eslint-disable-next-line no-return-assign
-        .then((data) => this.gifdata = data.data);
+        .then((data) => (this.gifdata = data.data));
     },
   },
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <HelloWorld msg="Busca un Gif"/>
+    <HelloWorld msg="🛸"/>
   </div>
 </template>
 
